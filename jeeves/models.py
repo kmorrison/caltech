ebb823eda059ac5909099de58e8bb3751aa81711
Kyle <fspoon64@gmail.com>
2012-09-09 18:44:57 -0700

    Initial commit

---
+++ jeeves/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib import admin
+
+class Interviewer(models.Model):
+
+    name = models.CharField(max_length=256)
+    domain = models.CharField(max_length=256)
+
+    def __unicode__(self):
+         return self.name
+
+
+class Requisition(models.Model):
+
+    name = models.CharField(max_length=256)
+    interviewers = models.ManyToManyField(Interviewer, related_name='requisitions')
+
+    def __unicode__(self):
+         return self.name
+
+class RequisitionInline(admin.TabularInline):
+    model = Requisition.interviewers.through
+
+class InterviewerAdmin(admin.ModelAdmin):
+    inlines = [RequisitionInline]
+
+class RequisitionAdmin(admin.ModelAdmin):
+    inlines = [RequisitionInline]
+    exclude = ('interviewers',)
+
+admin.site.register(Interviewer, InterviewerAdmin)
+admin.site.register(Requisition, RequisitionAdmin)
